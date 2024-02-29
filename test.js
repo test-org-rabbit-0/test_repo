1fd376d215c0c4353a7d382a8e0f52bcc15558f6
rabbit1test <161643175+rabbit1test@users.noreply.github.com>
2024-02-29 13:13:20 -0800

    Update test.js

--- test.js
+++ test.js
@@ -28,6 +28,8 @@ const result2 = multiply(3, 4);
 const result3 = divide(10, 0);
 const result4 = findLargestNumber([1, 7, 4, 9, 3]);
 
+const result5 - result1 + result2 - result3;
+
 console.log(result1);
 console.log(result2);
 console.log(result3);
